892f5b17ac99d8d911a519a0dd913047ecfe5077
KelvenFigueiredo <kelven.de.figueiredo.leopoldo@gmail.com>
2024-02-26 11:42:43 +0200

    primeiros componentes

---
+++ rate/src/Components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Main(){
+    return(
+        <>
+            <main>
+                <h1> Main</h1>
+            </main>
+        </>
+    );
+}
